5cb3dc4d2ab9e61be20190da43685bcd158f0827
Jacob Schroder <jbschroder@unm.edu>
2023-12-20 10:48:27 -0700

    couple tweaks to autograder, regarding pandas loading an unloading data

--- autograder/autograder.py
+++ autograder/autograder.py
@@ -5,7 +5,14 @@ import pandas as pd
 
 def grade_homework(TestClass, csv_filename, test_names, test_points, test_feedbacks):
     ## Copy Roster to Initial Homework0 Grade CSV File
-    df = pd.read_csv(csv_filename)
+
+    # When reading from CSV, two things to pay attention to.
+    #  - The first column has no header, so we have to tell pandas that this is the index column
+    #  - Force student ID to be a string for printing to file.  The ID is really an integer,
+    #    but the ID integers are too big to fit into a regular int with max value of 32K.
+    #    By default, pandas will treat the ID as a string
+    df = pd.read_csv(csv_filename, index_col=[0], dtype={"ID": str})
+
 
     hw_scores = list()
     hw_feedback = list()
@@ -68,9 +75,13 @@ def grade_homework(TestClass, csv_filename, test_names, test_points, test_feedba
             col_name = header
             break
 
-
+    # store grades in df
     for i in range(len(hw_scores)):
         df.at[i+1, col_name] = hw_scores[i]
 
+    # save updated grades to csv file
+    df.to_csv(csv_filename)
+
+    # write separate feedback file
     df = pd.DataFrame(data={'Name' : names[1:], 'Score' : hw_scores, 'Feedback' : hw_feedback})
     df.to_csv("feedback.csv")
