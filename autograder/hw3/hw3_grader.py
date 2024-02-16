877b1f067f08531aedd54ce2431aa143d0652b5d
Jacob Schroder <jbschroder@unm.edu>
2024-02-15 17:51:05 -0700

    updating object lectures and first object lab

--- autograder/hw3/hw3_grader.py
+++ autograder/hw3/hw3_grader.py
@@ -5,10 +5,10 @@ sys.path.append("..")
 import autograder
 
 # UPDATE:  Homework# here and below (4 total times)
-class TestHomework2(unittest.TestCase):
+class TestHomework3(unittest.TestCase):
 
     def __init__(self, test_name, filename):
-        super(TestHomework2, self).__init__(test_name)
+        super(TestHomework3, self).__init__(test_name)
         self.filename = filename
 
     ## setUp method is run before each test
@@ -42,28 +42,52 @@ class TestHomework2(unittest.TestCase):
         self.assertTrue( turtle.start_location == (0,0), msg = "Turtle start location changed from (0,0) ")
         self.assertTrue( turtle.pond_location == (12,12), msg = "Turtle pond location changed from (12,12) ")
         self.assertTrue( turtle.maze_number == 1, msg = "Turtle maze number should be 1")
-        self.assertTrue( turtle.number_of_turtles == 1, msg = "There should only be 1 turtle")
+        self.assertTrue( turtle.number_of_turtles == 2, msg = "There should be 2 turtles")
 
         with open(self.filename+".py") as myfile:
+            # Make file all lower case, and strip all spaces
             file_contents = myfile.read().lower().replace(' ', '')
             self.assertTrue("turtle=turtle_generator(maze_number=" in file_contents, msg = "Where is the turtle_generator line?") 
             self.assertTrue("turtle.start_new_journey()" in file_contents, msg = "Where is the start_new_journey line?") 
-            self.assertTrue("turtle.move_right()" in file_contents, msg = "Where are your move_right lines?") 
-            self.assertTrue("turtle.move_up()" in file_contents, msg = "Where are your move_up lines?") 
-            self.assertTrue("turtle.check_maze_completed()" in file_contents, msg = "Where is your check_maze_completed line?") 
+            self.assertTrue("turtle.move_right(which_turtle=" in file_contents, msg = "Where are your move_right lines?") 
+            self.assertTrue("turtle.move_up(which_turtle=" in file_contents, msg = "Where are your move_up lines?") 
+            self.assertTrue("turtle.check_maze_completed(which_turtle=" in file_contents, msg = "Where is your check_maze_completed line?") 
             self.assertTrue("turtle.save_everything_to_file()" in file_contents, msg = "Where is your save_everything_to_file line?") 
         
 
 
-    def test_turtle_tasks(self):
+    def test_turtles_complete_maze(self):
         #import pdb; pdb.set_trace()
         
         import pickle
         filehandler = open('turtle.pickle', 'rb')
         turtle = pickle.load(filehandler)
         
-        self.assertTrue(turtle.check_maze_completed() == True, msg = "Your turtle did not successfully complete maze, turtle.check_maze_completed() returned False")
+        self.assertTrue(turtle.check_maze_completed() == True, msg = "Your turtle 0 did not successfully complete maze, turtle.check_maze_completed(which_turtle=0) returned False")
+        self.assertTrue(turtle.check_maze_completed() == True, msg = "Your turtle 1 did not successfully complete maze, turtle.check_maze_completed(which_turtle=1) returned False")
+    
+    def test_alternating_trail(self):
+        import pickle
+        filehandler = open('turtle.pickle', 'rb')
+        turtle = pickle.load(filehandler)
+        
+        moves0, trail0 = turtle.turtles[0].get_movements_and_trail()
+        moves1, trail1 = turtle.turtles[1].get_movements_and_trail()
 
+        correct_trail0 = [False, True, False, True, False, True, False, True, False, True, False, True, False, \
+                            True, False, True, False, True, False, True, False, True, False, True, False]
+        correct_trail1 = [False, False, True, False, True, False, True, False, True, False, True, False, True, \
+                            False, True, False, True, False, True, False, True, False, True, False, True]
+        
+        # Allow them to alternate in both ways
+        correct_trails = False
+        if (trail0 == correct_trail0) and (trail1 == correct_trail1):
+            correct_trails = True
+        elif (trail0 == correct_trail1) and (trail1 == correct_trail0):
+            correct_trails = True
+
+        self.assertTrue(correct_trails == True, msg = "Turtles left an incorrect trail")
+        
 
     def test_cleanup(self):
         # remove turtle file
@@ -76,8 +100,8 @@ class TestHomework2(unittest.TestCase):
 
 if __name__=='__main__':
     # UPDATE: If you add a new test function, you have to add it in the below test_ lists 
-    test_names = ['test_load', 'test_turtle_loading', 'test_turtle_tasks', 'test_cleanup']
-    test_points = [15, 65, 20, 0]
-    test_feedbacks = ['Cannot load .py file', 'Turtle does not load properly', 'Turtle does not complete maze', 'No turtle file created']
+    test_points =    [15,                            25,                            30,                             30,                            0]
+    test_names =     ['test_load',            'test_turtle_loading',           'test_turtles_complete_maze',    'test_alternating_trail',      'test_cleanup']
+    test_feedbacks = ['Cannot load .py file', 'Turtle does not load properly', 'Turtle does not complete maze', 'Alternating trail incorrect', 'No turtle file created']
     # UPDATE: You need to update homework number in two places below
-    autograder.grade_homework(TestHomework2, 2, test_names, test_points, test_feedbacks)
+    autograder.grade_homework(TestHomework3, 3, test_names, test_points, test_feedbacks)
